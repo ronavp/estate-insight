8606ddb6be31a8eec59e1ecfb865759cdcc879df
ronp9 <ronp426@outlook.com>
2025-05-10 11:35:16 +1000

    fixed some linting error with importing model module

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,7 +1,8 @@
 """Intialises the tables for the db"""
-from models import Base, engine 
+from .models import Base, engine  
 
 # Create all tables defined in the model (properties table)
-Base.metadata.create_all(bind=engine)
-print("Estate-Insight Databse tables created successfully")
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    print("Estate-Insight Databse tables created successfully")
 
